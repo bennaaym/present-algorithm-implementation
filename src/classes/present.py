e7873a6fe3fa9f9600783072800efbb5fcf64371
bennaaym <mr.aymen.bennabi@gmail.com>
2022-12-10 17:37:52 +0300

    :construction: implement PRESENT ecrypt & decrypt

---
+++ src/classes/present.py
@@ -0,0 +1,37 @@
+from src.classes.key_scheduler import KeySchedular
+from src.classes.add_round_key import AddRoundKeyLayer
+from src.classes.s_box_layer import SBoxLayer
+from src.classes.p_layer import PLayer
+from src.classes.util import Util
+import codecs
+
+
+class PRESENT:
+  rounds = 32
+  def __init__(self, key: str) -> None:
+    self.__key_schedular = KeySchedular(key)
+  
+  def encrypt(self, plain_text: str) -> str:
+    state: int = Util.string_to_number(plain_text)
+    for i in range(self.rounds - 1):
+      state = AddRoundKeyLayer.add_round_key(state, self.__key_schedular.round_keys[i])
+      state = SBoxLayer.substitute(state)
+      state = PLayer.permute(state)
+    
+    cipher_text = AddRoundKeyLayer.add_round_key(state, self.__key_schedular.round_keys[-1])
+    cipher_text =  Util.number_to_string(cipher_text, 8)
+    cipher_text = codecs.encode(cipher_text, "hex")
+    return str(str(cipher_text)[2:-1])
+
+  
+  def decrypt(self, cipher_text: str) -> str:
+    state: int = Util.string_to_number(cipher_text)
+    for i in range(self.rounds - 1):
+      state = AddRoundKeyLayer.add_round_key(state, self.__key_schedular.round_keys[-i - 1])
+      state = PLayer.inverse_permute(state)
+      state = SBoxLayer.inverse_substitute(state)
+    
+    plain_text = AddRoundKeyLayer.add_round_key(state, self.__key_schedular.round_keys[0])
+    plain_text = Util().number_to_string(plain_text, 8)
+    plain_text = codecs.encode(plain_text, "hex")
+    return plain_text
